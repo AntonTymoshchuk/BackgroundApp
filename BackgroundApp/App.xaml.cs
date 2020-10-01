195fae1b45e193a585c08314e3deb973c59cd452
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-01 18:25:20 +0300

    Add project files.

---
+++ BackgroundApp/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace BackgroundApp
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow = new MainWindow();
+            MainWindow.Closing += MainWindow_Closing;
+
+            notifyIcon = new System.Windows.Forms.NotifyIcon();
+            notifyIcon.MouseClick += NotifyIcon_MouseClick;
+            notifyIcon.Text = "Launcher";
+            notifyIcon.Icon = new Icon(@"C:\Users\Lenovo\Documents\icon.ico");
+            notifyIcon.Visible = true;
+        }
+
+        private void NotifyIcon_MouseClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+                MainWindow.Show();
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            MainWindow.Hide();
+        }
+    }
+}
